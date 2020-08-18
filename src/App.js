30469d3cafa22f1f9c48e6cef0da4b24b40c5d4f
Madeline Uribes <madeline.uribes@gmail.com>
2020-08-17 22:51:49 -0400

    Adding styles, Creating Search Input

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Header = () => (
+  <div className="header grid">
+    <h1 className="title">Jiffy</h1>
+  </div>
+);
+
+class App extends Component {
+  render() {
+    return (
+      <div className="page">
+        <Header />
+        <div className='search grid'>
+          {}
+          <imput className='input grid-item' placeholder="Type something" />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
