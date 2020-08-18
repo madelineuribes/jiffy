d96b5b4280acbf99ace91c14651ea09ff8fb9fbd
Madeline Uribes <madeline.uribes@gmail.com>
2020-08-18 13:38:06 -0400

    Created gif player component

--- src/App.js
+++ src/App.js
@@ -20,7 +20,8 @@ class App extends Component {
     super(props)
     this.state = {
       searchTerm: '',
-      hintText: ''
+      hintText: '',
+      gif: null
     };
   }
 
@@ -29,10 +30,17 @@ class App extends Component {
     //fetch
     try {
       //use await keyword to wait for response to come back
-      const response = await fetch('https://api.giphy.com/v1/gifs/search?api_key=47XQWNYAyTmR6PwYvVWYHtCmxLLQSg1v&q=&limit=25&offset=0&rating=g&lang=en');
+      const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=47XQWNYAyTmR6PwYvVWYHtCmxLLQSg1v&q=
+      ${searchTerm}
+      &limit=25&offset=0&rating=g&lang=en`);
       //convert response into json
-      const data = await response.json();
-      console.log(data);
+      const { data } = await response.json();
+
+      this.setState((prevState, props) => ({
+        ...prevState,
+        gif: data[0]
+      }));
+
       // if fetch fails, catch it 
     } catch (error) {
 
@@ -62,12 +70,16 @@ class App extends Component {
   };
 
   render() {
-    const { searchTerm } = this.state;
+    const { searchTerm, gif } = this.state;
     return (
       <div className="page">
         <Header />
+
         <div className='search grid'>
-          {}
+
+          {gif && <video className='grid-item video' autoPlay loop
+            src={gif.images.original.mp4} />}
+
           <input className='input grid-item' placeholder="Type something" onChange={this.handleChange}
             onKeyPress={this.handleKeyPress}
             value={searchTerm} />
